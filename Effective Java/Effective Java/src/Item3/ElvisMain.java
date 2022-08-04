7f86af6a1574dc4d5d19c1d5c1fa94a54927e3b6
Dheyeong Tchalla <dheyeongtchalla@gmail.com>
2022-08-04 11:15:37 +0900

    no message

--- Effective Java/Effective Java/src/Item3/ElvisMain.java
+++ Effective Java/Effective Java/src/Item3/ElvisMain.java
@@ -4,7 +4,11 @@ public class ElvisMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Elvis.INSTANCE.toString();
+		Elvis.INSTANCE.leavetheBuilding();
+		
+		//Elvis with factory method
+		ElvisFactory.getInstance();
 	}
 
 }
